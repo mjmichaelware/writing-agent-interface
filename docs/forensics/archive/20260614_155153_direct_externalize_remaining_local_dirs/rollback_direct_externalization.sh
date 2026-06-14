c186b162089b0587fa297f880ca889760bba8e79
mjmichaelware <michaelware433@yahoo.com>
2026-06-14 15:51:59 -0600

    Directly externalize remaining local-only directories

---
+++ docs/forensics/archive/20260614_155153_direct_externalize_remaining_local_dirs/rollback_direct_externalization.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set +e
+cd '/data/data/com.termux/files/home/Workspaces/Hybrid/writing-agent-interface'
+mkdir -p '/data/data/com.termux/files/home/Workspaces/Hybrid/writing-agent-interface/data'
+mv '/data/data/com.termux/files/home/.nos_private/writing-agent-interface/source_archive__from_repo_20260614_215157' '/data/data/com.termux/files/home/Workspaces/Hybrid/writing-agent-interface/data/source_archive'
+mkdir -p '/data/data/com.termux/files/home/Workspaces/Hybrid/writing-agent-interface'
+mv '/data/data/com.termux/files/home/.nos_private/writing-agent-interface/secrets' '/data/data/com.termux/files/home/Workspaces/Hybrid/writing-agent-interface/secrets'
